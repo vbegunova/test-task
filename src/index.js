a21ec268b96cb6e445428a0eb038cc2fe16b4761
vbegunova <118359000+vbegunova@users.noreply.github.com>
2023-11-10 23:37:29 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
 import './index.css';
+import { Provider } from 'react-redux';
+import { store } from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='test-task'>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter basename="test-task">
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
