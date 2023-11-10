ef95d224eb6f79ffe55ebdfc34d9eca39d9bbe23
vbegunova <118359000+vbegunova@users.noreply.github.com>
2023-11-10 21:38:40 +0000

    Deploying to gh-pages from @ vbegunova/test-task@a21ec268b96cb6e445428a0eb038cc2fe16b4761 🚀

--- static/js/77.b27e797c.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[77],{77:function(e,t,r){r.r(t),r.d(t,{default:function(){return a}});var n=r(184),a=function(){return(0,n.jsx)("div",{children:"Catalog"})}}}]);
-//# sourceMappingURL=77.b27e797c.chunk.js.map
