d5f2a29faf0854bf3af4070359b2bf526ba90d18
vbegunova <118359000+vbegunova@users.noreply.github.com>
2023-11-10 23:36:22 +0200

    Add loader

---
+++ src/components/Loader/Loader.styled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: block;
+  margin-top: 20px;
+  margin-left: auto;
+  margin-right: auto;
+  width: 30px;
+`;
+
+export { Container };
