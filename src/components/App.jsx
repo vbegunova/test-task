bd4c003ba50dfab7ba2b1ae9fc548fd7bd475980
vbegunova <118359000+vbegunova@users.noreply.github.com>
2023-11-12 18:56:26 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,20 @@
-import { lazy } from 'react';
+import { lazy, useEffect } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import SharedLayout from './SharedLayout';
+import { useDispatch } from 'react-redux';
+import { fetchAllAdverts } from 'redux/operations';
 
 const Home = lazy(() => import('../pages/Home'));
 const Catalog = lazy(() => import('../pages/Catalog'));
 const Favorites = lazy(() => import('../pages/Favorites'));
 
 export const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchAllAdverts());
+  }, [dispatch]);
+
   return (
     <Routes>
       <Route path="/" element={<SharedLayout />}>
