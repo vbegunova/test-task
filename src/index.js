a5ced2b049007036649f95a1685e2f3b6f8e4e47
vbegunova <118359000+vbegunova@users.noreply.github.com>
2023-11-12 23:44:27 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import { App } from 'components/App';
 import './index.css';
 import { Provider } from 'react-redux';
 import { store } from 'redux/store';
+import { GlobalStyle } from 'GlobalStyles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter basename="test-task">
+        <GlobalStyle />
         <App />
       </BrowserRouter>
     </Provider>
