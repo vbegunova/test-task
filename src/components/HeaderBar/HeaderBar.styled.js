93172725cb0cd4472cb0cde58a2f9261f344a70b
vbegunova <118359000+vbegunova@users.noreply.github.com>
2023-11-12 23:44:38 +0200

    Update HeaderBar.styled.js

--- src/components/HeaderBar/HeaderBar.styled.js
+++ src/components/HeaderBar/HeaderBar.styled.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
 
 export const Header = styled.header`
-  margin-bottom: 60px;
+  margin-bottom: 80px;
   box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
     0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
 `;
