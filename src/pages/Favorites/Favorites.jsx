f50722af64a901e16908a1395dc444dce53ebc3f
vbegunova <118359000+vbegunova@users.noreply.github.com>
2023-11-12 18:58:09 +0200

    Add Favorites

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -1,6 +1,41 @@
+import { useSelector } from 'react-redux';
+import { selectFavoritesAdverts } from 'redux/selectors';
+import { useState } from 'react';
+import Modal from 'components/Modal/Modal';
+import ModalContent from 'components/ModalContent/ModalContent';
+import CatalogList from 'components/CatalogList/CatalogList';
+
 const Favorites = () => {
-    return <div>Favorites</div>;
+  const favoriteAdverts = useSelector(selectFavoritesAdverts);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  const openModal = item => {
+    setIsModalOpen(true);
+    setSelectedItem(item);
+    document.body.style.overflow = 'hidden';
   };
-  
-  export default Favorites;
-  
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setSelectedItem(null);
+    document.body.style.overflow = 'auto';
+  };
+
+  return (
+    <>
+      {favoriteAdverts[0] ? (
+        <CatalogList adverts={favoriteAdverts} openModal={openModal} />
+      ) : (
+        <p>There are no favorites</p>
+      )}
+      {isModalOpen && (
+        <Modal onClose={closeModal}>
+          <ModalContent advert={selectedItem} />
+        </Modal>
+      )}
+    </>
+  );
+};
+
+export default Favorites;
