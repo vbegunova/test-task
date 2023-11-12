46d5ae1bfc91ddc2fe4f083210dcaf060b4e7568
vbegunova <118359000+vbegunova@users.noreply.github.com>
2023-11-12 23:46:41 +0200

    Update Filter.styled.js

--- src/components/Filter/Filter.styled.js
+++ src/components/Filter/Filter.styled.js
@@ -9,11 +9,29 @@ export const ButtonSearch = styled.button`
   font-weight: 600;
   line-height: 1.43;
   border: none;
+  transition: background-color 300ms cubic-bezier(0.075, 0.82, 0.165, 1);
+
+  &:hover {
+    cursor: pointer;
+    background-color: #0b44cd;
+  }
 `;
 
 export const FilterContainer = styled.div`
+  position: relative;
   margin-bottom: 50px;
   display: flex;
   gap: 18px;
   justify-content: center;
+`;
+
+export const LabelFilter = styled.p`
+  position: absolute;
+  z-index: 1000;
+  top: -22px;
+  left: 402px;
+  color: #8a8a89;
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 1.3;
 `;
