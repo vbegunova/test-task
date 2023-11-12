2962762aef7781f1bcca65382613afcd00bd5633
vbegunova <118359000+vbegunova@users.noreply.github.com>
2023-11-12 18:57:16 +0200

    Update selectors.js

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,4 +1,15 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 export const selectAdverts = state => state.adverts.items;
 export const selectQuantity = state => state.adverts.quantity;
 export const selectIsLoading = state => state.adverts.isLoading;
 export const selectError = state => state.adverts.error;
+export const selectFavoriteIds = state => state.adverts.favorites;
+export const selectBDItems = state => state.adverts.bditems;
+
+export const selectFavoritesAdverts = createSelector(
+  [selectBDItems, selectFavoriteIds],
+  (adverts, favoriteIds) => {
+    return adverts.filter(advert => favoriteIds.includes(advert.id));
+  }
+);
