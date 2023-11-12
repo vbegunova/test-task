112c7cf5986fefe015d68f9fecc8294f5a71dbf3
vbegunova <118359000+vbegunova@users.noreply.github.com>
2023-11-12 18:58:29 +0200

    Create GlobalStyles.jsx

---
+++ src/GlobalStyles.jsx
@@ -0,0 +1,35 @@
+import 'modern-normalize';
+import { createGlobalStyle } from 'styled-components';
+import ManropeMedium from './fonts/Manrope-Medium.woff2';
+import ManropeRegular from './fonts/Manrope-Regular.woff2';
+import ManropeSemiBold from './fonts/Manrope-Semibold.woff2';
+
+export const GlobalStyle = createGlobalStyle`
+    @font-face {
+        font-family: 'Manrope';
+        src: url(${ManropeMedium}) format('woff2'),
+            url(${ManropeRegular}) format('woff2'),
+            url(${ManropeSemiBold}) format('woff2');
+    }
+
+    &.hidden {
+    position: absolute;
+    white-space: nowrap;
+    width: 1px;
+    height: 1px;
+    overflow: hidden;
+    border: 0;
+    padding: 0;
+    clip: rect(0 0 0 0);
+    clip-path: inset(50%);
+    margin: -1px;
+    }
+
+    *,
+    *::before,
+    *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    }
+`;
